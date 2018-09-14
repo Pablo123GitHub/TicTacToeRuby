0bd58f1c8afaa155d7d10a19ecf7bb66eb776318
Pablo <paul.ladiv123@gmail.com>
2018-09-14 17:07:30 +0100

    add a test to check that the array contains answers at the right index

--- lib/grid.rb
+++ lib/grid.rb
@@ -6,12 +6,13 @@ class Grid
     @answers_array = Array.new(9, "")
   end
 
+  def records(index, cross_or_nought)
+    raise 'Invalid index' unless (0..8).include?(index)
+    raise 'Index spot already taken' unless @answers_array[index].empty?
+    @answers_array[index] = cross_or_nought
+  end
 
-  # def records(index, cross_or_nought)
-  #   raise 'Index spot already taken' if @answers_array.include? index
-  #   raise 'Invalid index' unless (0..8).include?(index)
-  #   @answers_array[index] = cross_or_nought
-  # end
+
 
 
 
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -7,30 +7,24 @@ describe Grid do
       expect(subject.answers_array).to eq(Array.new(9, ""))
     end
 
-    # it "records answers total of 1 after recording one answer" do
-    #   subject.records(1)
-    #   expect(subject.answers_total).to eq(1)
-    # end
-  #
-  #   it "throws an error when trying to record at the same index x times" do
-  #     subject.records(1)
-  #     expect { subject.records(1) }.to raise_error("Index spot already taken")
-  #   end
-  #
-  #   it "checks that the input is more than 0 and less or equal than 9" do
-  #     expect{ subject.records(9)}.to raise_error("Invalid index")
-  #     expect{ subject.records(-1)}.to raise_error("Invalid index")
-  #   end
-  # end
-  #
-  # context "allocates the choices to each side playing" do
-  #   it "allocates first decision to first player" do
-  #     subject.records(4)
-  #     subject.records(5)
-  #     expect(subject.show_first_player_decisions).to eq(["","","","","","","","",""])
-  #   end
-  #
-  #
+
+    it "throws an error when trying to record at the same index x times" do
+      subject.records(1, "X")
+      expect { subject.records(1, "X") }.to raise_error("Index spot already taken")
+    end
+
+    it "checks that the input is more than 0 and less or equal than 8" do
+      expect{ subject.records(9, "X")}.to raise_error("Invalid index")
+      expect{ subject.records(-1, "X")}.to raise_error("Invalid index")
+    end
+
+  context "allocates the choices to each side playing" do
+    it "allocates first decision to first player" do
+      subject.records(4, 'X')
+      subject.records(5, 'X')
+      expect(subject.answers_array).to eq(["","","","","X","X","","",""])
+    end
+  end
   end
 
 end
