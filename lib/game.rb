9e9dc9705883546bc930f6cb00eab127d026d0ec
Pablo <paul.ladiv123@gmail.com>
2018-10-18 10:59:45 +0100

    add is_finished to the Game class (along with relevant tests)

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,17 @@
-require('grid')
 
 class Game
 
+  def initialize (grid)
+    @grid = grid.new
+  end
 
-def start
-  puts 'Please start the game'
-end
+  def start
+    puts "select a number between 0 and 8 \nthen press Enter"
+  end
 
+  def is_finished
+    @grid.is_full
+  end
 
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,13 +1,27 @@
 require('game')
 
 describe Game do
+
   context "gives intructions about how to play the game" do
+
     it "tells the human player to start the game" do
-      expect(STDOUT).to receive(:puts).with('Please start the game')
-      subject.start
+      grid = double("grid")
+      grid.stub(:new)
+      game = Game.new(grid)
+      expect(STDOUT).to receive(:puts).with("select a number between 0 and 8 \nthen press Enter")
+      game.start
+    end
+  end
+
+  context "tracks events throughout the game" do
+    it "tells the game is NOT finished if grid is NOT full and no one wins yet" do
+      grid = double("grid")
+      grid.stub(:new)
+      @grid.stub(:is_full).and_return(false)
+      game = Game.new(grid)
+      expect(game.is_finished).to eq(false)
     end
 
-
-
   end
+
 end
