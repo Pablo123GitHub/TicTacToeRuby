4e629f1ccfaf1d40f389dd14ed7fdb604eb16abf
Pablo <paul.ladiv123@gmail.com>
2018-11-08 15:09:08 +0000

    refactoring the gameSpec file

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,7 +5,6 @@ require_relative '../spec/factories'
 
 
 describe Game do
-  include Factories
 
   subject { described_class.new(grid) }
 
@@ -24,7 +23,6 @@ describe Game do
       before(:each) do
         allow(grid).to receive(:is_full).and_return(true)
       end
-      # let(:grid) { create_full_grid }
 
       it "is finished When one side wins" do
         allow(grid).to receive(:one_side_wins).and_return(true)
@@ -51,31 +49,6 @@ describe Game do
         allow(grid).to receive(:one_side_wins).and_return(false)
         expect(subject.is_finished).to eq(false)
       end
-
     end
-
-  end
-
-  context "records input and adds a random play from the computer" do
-    # it "outputs a random number between 1 and 10" do
-    #     grid = double("grid")
-    #     grid.stub(:new)
-    #     game = Game.new(grid)
-    #     expect(game).to receive(:rand).and_return(8)
-    #     expect(game.random).to eq 8
-    #
-    # end
-  #   it "records a computer play as soon as the human plays a move" do
-  #     grid = double("grid")
-  #     grid.stub(:new)
-  #     game = Game.new(grid)
-  #     expect(game).to receive(:rand).and_return(4)
-  #     expect(game.records_input_and_computer_play(0)).to eq(4)
-  #     expect(game.grid.answers.array.length).to eq(2)
-  #
-  #
-  #   # end
-  # end
-
   end
   end
